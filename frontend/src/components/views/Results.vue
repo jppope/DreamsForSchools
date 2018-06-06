69f4ddad68c71224cf034af66d37d959e3465532
Jon Paul Uritis <jonpaul.uritis@thestormcloudgroup.com>
2018-06-05 17:04:04 -0700

    sorting that I forgot to do last night

--- frontend/src/components/views/Results.vue
+++ frontend/src/components/views/Results.vue
@@ -10,15 +10,19 @@
             <tr>
               <th>Team Name</th>
               <td>Judge Name</td>
-              <th v-for="n in 7">Question {{ n }}</th>
+              <th v-for="n in 8">Question {{ n }}</th>
+              <th>Total</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="score in event.scores">
+            <tr v-for="score in scored">
               <td>{{score.team.team_name}}</td>
               <td>{{score.judge.name}}</td>
               <td v-for="question in score.score">
                 {{ question }}
+              </td>
+              <td>
+                {{ totalScore(score.score) }}
               </td>
             </tr>
           </tbody>
@@ -34,6 +38,23 @@
   export default {
     computed: {
       ...mapGetters(['event']),
+      scored() {
+        return this.event.scores.sort((a, b) => {
+          if (a.team.team_name < b.team.team_name) return -1;
+          if (a.team.team_name > b.team.team_name) return 1;
+          return 0;
+        });
+      },
+    },
+    methods: {
+      totalScore(obj) {
+        return Object.values(obj)
+        .reduce((total, item) => {
+          // eslint-disable-next-line
+          total += parseInt(item);
+          return total;
+        }, 0);
+      },
     },
   };
 </script>
